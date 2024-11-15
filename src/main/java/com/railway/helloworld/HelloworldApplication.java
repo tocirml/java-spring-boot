92a14e50f0f7e37538552cca79c82a084887c802
Toci <tocirml2@gmail.com>
2024-11-15 15:22:45 -0600

    import fix

--- src/main/java/com/railway/helloworld/HelloworldApplication.java
+++ src/main/java/com/railway/helloworld/HelloworldApplication.java
@@ -1,4 +1,4 @@
-package com.railwayguide.helloworld;
+package com.railway.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
